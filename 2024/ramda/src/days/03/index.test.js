27a7be16faf8e30707ee1d7d31df8c31a94f6774
Lambdaphile <lambdaphile@pm.me>
2024-12-24 17:43:52 +0300

    [2024 - Ramda] Add day 3, part 1

---
+++ 2024/ramda/src/days/03/index.test.js
@@ -0,0 +1,20 @@
+import { parseInput, part1 } from './index.js'
+
+const input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+
+describe('parseInput', () => {
+  it('should convert the input into a two-dimensional array of numbers', () => {
+    expect(parseInput(input)).toEqual([
+      [2, 4],
+      [5, 5],
+      [11, 8],
+      [8, 5]
+    ])
+  })
+})
+
+describe('part1', () => {
+  it('should return the sum of uncorrupted mul instructions', () => {
+    expect(part1(input)).toBe(161)
+  })
+})
