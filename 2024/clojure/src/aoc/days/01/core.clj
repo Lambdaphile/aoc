a160bdee86b83273f5bb27b903cbc002031a56df
Lambdaphile <lambdaphile@pm.me>
2024-12-26 21:32:01 +0300

    [2024 - Clojure] Add initial Clojure setup

---
+++ 2024/clojure/src/aoc/days/01/core.clj
@@ -0,0 +1,10 @@
+(ns aoc.days.01.core
+  (:require [aoc.utils.core :as u]))
+
+(defn part-1 [input]
+  "Part 1")
+
+(defn part-2 [input]
+  "Part 2")
+
+(u/run "01" part-1 part-2)
---
+++ 2024/clojure/src/aoc/days/01/core_test.clj
@@ -0,0 +1,7 @@
+(ns aoc.days.01.core-test
+  (:require [clojure.test :refer :all]
+            [aoc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 0))))
