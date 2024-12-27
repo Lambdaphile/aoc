312d3c069832af1993ba097b63ded127f1cfadb4
Lambdaphile <lambdaphile@pm.me>
2024-12-27 22:44:36 +0300

    [2024 - Clojure] Add day 1

--- 2024/clojure/src/aoc/days/01/core.clj
+++ 2024/clojure/src/aoc/days/01/core.clj
@@ -1,10 +1,32 @@
 (ns aoc.days.01.core
   (:require [aoc.utils.core :as u]))
 
+(defn parse-input [input]
+  (->> input
+       (re-seq #"\d+")
+       (map parse-long)
+       (partition 2)
+       u/transpose))
+
+(defn abs-diff [x y]
+  (abs (- x y)))
+
+(defn similarities [[l r]]
+  (let [freq-map (frequencies r)]
+    (map #(* % (freq-map % 0)) l)))
+
 (defn part-1 [input]
-  "Part 1")
+  (->> input
+       parse-input
+       (map sort)
+       u/transpose
+       (reduce (fn [sum [x y]]
+                 (+ sum (abs-diff x y))) 0)))
 
 (defn part-2 [input]
-  "Part 2")
+  (->> input
+       parse-input
+       similarities
+       (reduce +)))
 
-(u/run "01" part-1 part-2)
+(u/run "01" part-1 part-2) ;; => 1197984 23387399
--- 2024/clojure/src/aoc/days/01/core_test.clj
+++ 2024/clojure/src/aoc/days/01/core_test.clj
@@ -1,7 +1,29 @@
 (ns aoc.days.01.core-test
   (:require [clojure.test :refer :all]
-            [aoc.core :refer :all]))
+            [aoc.days.01.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
+(def input "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(def parsed-input
+  [[3 4 2 1 3 3]
+   [4 3 5 3 9 3]])
+
+(deftest parse-input-test
+  (is (= parsed-input (parse-input input))))
+
+(deftest abs-diff-test
+  (is (= 1 (abs-diff 1 2))))
+
+(deftest similarities-test
+  (is (= [9 4 0 0 9 9] (similarities parsed-input))))
+
+(deftest part-1-test
+  (is (= 11 (part-1 input))))
+
+(deftest part-2-test
+  (is (= 31 (part-2 input))))
