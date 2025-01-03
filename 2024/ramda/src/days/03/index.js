49733691f0b1418f28dac1d1abf8a8445dcb788b
Lambdaphile <lambdaphile@pm.me>
2025-01-03 23:56:54 +0300

    [2024 - Ramda] Update day 3, part 1 and add part 2

--- 2024/ramda/src/days/03/index.js
+++ 2024/ramda/src/days/03/index.js
@@ -1,17 +1,29 @@
 import * as R from 'ramda'
 import { run } from '../../utils/index.js'
 
-export const parseInput = R.pipe(
-  R.match(/mul\(\d+,\d+\)/g),
-  R.map(R.match(/\d+/g)),
-  R.map(R.map(Number)),
+export const evaluate = R.pipe(
+  R.match(/\d+/g),
+  R.map(Number),
+  R.apply(R.multiply)
 )
-
-const sumProduct = R.reduce((sum, [a, b]) => sum + a * b, 0)
 
 export const part1 = R.pipe(
-  parseInput,
-  sumProduct
+  R.match(/mul\(\d+,\d+\)/g),
+  R.reduce((sum, expr) => sum + evaluate(expr), 0)
 )
 
-run(import.meta.url, part1)
+export const part2 = R.pipe(
+  R.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g),
+  R.reduce(([isEnabled, sum], expr) =>
+    R.cond([
+      [R.equals('do()'), R.always([true, sum])],
+      [R.equals('don\'t()'), R.always([false, sum])],
+      [R.always(isEnabled), R.always([true, sum + evaluate(expr)])],
+      [R.T, R.always([false, sum])]
+    ])(expr),
+    [true, 0]
+  ),
+  R.last
+)
+
+run(import.meta.url, part1, part2)
--- 2024/ramda/src/days/03/index.test.js
+++ 2024/ramda/src/days/03/index.test.js
@@ -1,21 +1,22 @@
-import { parseInput, part1 } from './index.js'
+import { evaluate, part1, part2 } from './index.js'
 
-const input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-const parsedInput = [
-  [2, 4],
-  [5, 5],
-  [11, 8],
-  [8, 5]
-]
+const input1 = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const input2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
-describe('parseInput', () => {
-  it('should convert the input into a two-dimensional array of numbers', () => {
-    expect(parseInput(input)).toEqual(parsedInput)
+describe('evaluate', () => {
+  it('should evaluate and return the result of a mul expression', () => {
+    expect(evaluate('mul(2,2)')).toBe(4)
   })
 })
 
 describe('part1', () => {
   it('should return the sum of uncorrupted mul instructions', () => {
-    expect(part1(input)).toBe(161)
+    expect(part1(input1)).toBe(161)
+  })
+})
+
+describe('part2', () => {
+  it('should return the sum of uncorrupted mul instructions inside do() and don\'t() instructions', () => {
+    expect(part2(input2)).toBe(48)
   })
 })
